b09887ad194ea2d73043dc279896e6fe9d1cee34
F4995875f <mavlonovavazbek1112@gmail.com>
2021-10-07 10:33:34 +0500

    Ikkinchi qadam

--- dict.py
+++ dict.py
@@ -1,8 +1,11 @@
-
+import os
 class Our_dictionary:
     def __init__(self):
         self.eng_word = None
         self.uz_word = None
+
+    def system_intranse(self):
+        pass
 
     def add_new_word(self):
         pass
@@ -15,3 +18,18 @@ class Our_dictionary:
 
     def go_out(self):
         pass
+
+    @staticmethod
+    def print_messege():
+        print("""
+        Add new word [1]
+        Search word  [2]
+        Show word    [3]
+        Go out       [4]
+        """)
+
+    @staticmethod
+    def cls():
+        os.system("clear")
+
+dict1 = Our_dictionary()
