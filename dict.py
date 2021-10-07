f6145fa4e2bc6ffbe5686dc28d95642e5f219456
F4995875f <jorayevfayzillo77@gmail.com>
2021-10-07 10:40:41 +0500

    3 qadam

--- dict.py
+++ dict.py
@@ -1,11 +1,24 @@
 import os
+
+
 class Our_dictionary:
     def __init__(self):
         self.eng_word = None
         self.uz_word = None
 
-    def system_intranse(self):
-        pass
+    def system_entrance(self):
+        self.cls()
+        options = ['1', '2', '3', '4']
+        self.print_messege()
+        input_choice = input(">>>: ").strip()
+        while self.is_string_empty(input_choice) or input_choice not in options:
+            self.cls()
+            self.print_messege()
+            input_choice = input(">>>: ").strip()
+
+        if input_choice == options[0]:
+            return
+
 
     def add_new_word(self):
         pass
@@ -32,4 +45,10 @@ class Our_dictionary:
     def cls():
         os.system("clear")
 
+    @staticmethod
+    def is_string_empty(str_):
+        return not bool(str_)
+
+
 dict1 = Our_dictionary()
+dict1.system_entrance()
