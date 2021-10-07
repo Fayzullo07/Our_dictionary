04373b8859fb31498d4bdfbac761de2ed349b03d
F4995875f <jorayevfayzillo77@gmail.com>
2021-10-07 10:26:28 +0500

    boshladik

---
+++ dict.py
@@ -0,0 +1,17 @@
+
+class Our_dictionary:
+    def __init__(self):
+        self.eng_word = None
+        self.uz_word = None
+
+    def add_new_word(self):
+        pass
+
+    def show_words(self):
+        pass
+
+    def search_word(self):
+        pass
+
+    def go_out(self):
+        pass
