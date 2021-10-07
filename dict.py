edfff2f17bbd2edc49732438207726f4968a7648
F4995875f <mavlonovavazbek1112@gmail.com>
2021-10-07 11:15:41 +0500

    6 qadam

--- dict.py
+++ dict.py
@@ -1,4 +1,13 @@
 import os
+import mysql.connector
+
+
+
+
+
+
+
+
 
 
 class Our_dictionary:
@@ -32,7 +41,19 @@ class Our_dictionary:
             self.cls()
             input_eng_word = input("English: ").strip()
 
-        input_uz_word = input("Uzbek: ")
+        input_uz_word = input("Uzbek: ").strip()
+        while self.is_string_empty(input_uz_word) or not input_uz_word.isalpha():
+            self.cls()
+            input_uz_word = input("Uzbek: ").strip()
+
+        self.eng_word = input_eng_word
+        self.uz_word = input_uz_word
+
+        mydb = self.entrance_database()
+        mycursor = mydb.cursor()
+        mycursor.execute(f"insert into words(English, Uzbek)values('{self.eng_word}', '{self.uz_word}')")
+        mydb.commit()
+
     def show_words(self):
         pass
 
@@ -59,6 +80,13 @@ class Our_dictionary:
     def is_string_empty(str_):
         return not bool(str_)
 
+    def entrance_database(self):
+        return mysql.connector.connect(
+            host="localhost",
+            user="Avazbek",
+            password="12345678",
+            database="lugat"
+        )
 
 dict1 = Our_dictionary()
 dict1.system_entrance()
